ad2fcae83acbe663e30795ac30cf60dae1c25c5d
Monica Szabo <monicaszb@gmail.com>
2016-07-21 11:13:31 -0500

    fixed mouseover on portfolio

--- assets/js/webpage.js
+++ assets/js/webpage.js
@@ -2,19 +2,21 @@ $(document).ready(function() {
 
     //On mouseover on the portfolio images, will show the correct caption
     $('.portPic').on('mouseover', function() {
+        console.log("from mouse over ",this)
         var id = "#" + $(this).attr('id');
         var idNum = id.slice(-2);
         var capID = "#caption" + idNum
-        console.log(capID + " this is in mouseover");
+
         $(capID).css('visibility', 'inherit');
 
     });
 
     $('.caption').on('mouseleave', function() {
+        console.log("from mouse leave",this)
         var id = "#" + $(this).attr('id');
         var idNum = id.slice(-2);
         var capID = "#caption" + idNum
-        console.log(capID + " this is in mouseleave");
+
         $(capID).css('visibility', 'hidden');
     });
     
