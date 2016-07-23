ef210ef475b80bef0b04f1531630529d1bde4956
Loris Bettazza <loriteck@yahoo.it>
2016-07-23 16:05:19 +0200

    Added missing semicolons (minor)

--- assets/js/webpage.js
+++ assets/js/webpage.js
@@ -2,20 +2,20 @@ $(document).ready(function() {
 
     //On mouseover on the portfolio images, will show the correct caption
     $('.portPic').on('mouseover', function() {
-        console.log("from mouse over ",this)
+        console.log("from mouse over ",this);
         var id = "#" + $(this).attr('id');
         var idNum = id.slice(-2);
-        var capID = "#caption" + idNum
+        var capID = "#caption" + idNum;
 
         $(capID).css('visibility', 'inherit');
 
     });
 
     $('.caption').on('mouseleave', function() {
-        console.log("from mouse leave",this)
+        console.log("from mouse leave",this);
         var id = "#" + $(this).attr('id');
         var idNum = id.slice(-2);
-        var capID = "#caption" + idNum
+        var capID = "#caption" + idNum;
 
         $(capID).css('visibility', 'hidden');
     });
