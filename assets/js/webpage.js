93b06068ad78c8aa08f2eb50c190b4718a3ab000
Monica Szabo <monicaszb@gmail.com>
2017-03-22 13:22:07 -0500

    fixed wide width issue

--- assets/js/webpage.js
+++ assets/js/webpage.js
@@ -21,24 +21,36 @@ $(document).ready(function() {
 
     //Height stays 100% of the screen
     function windowH() {
-         var wH = $(window).height();
+        var wH = $(window).height();
 
-         var wW = $(window).width();
+        var wW = $(window).width();
 
-         //Mobile responsiveness
-         if(wW <= 767) {
-            wH = 200;
-            $('.right-bar').css({'overflow-y': 'inherit'});
-         } else {
-            $('.right-bar').css({'overflow-y': 'scroll'});
-         }
+        //Mobile responsiveness
+        // if(wH <= 817) {
+        //    $('.border').css({'overflow-y': 'inherit'});
+        // } else {
+        //    $('.border').css({'overflow-y': 'scroll'});
+        // }
 
-         $('.background').css({height: wH});
+        var borderHeight = wH * .50;
+        var innerBlock = borderHeight - 100;
+
+        if(wW > 1389) {
+            var innerBlockWidth = wW * .74;
+            $('.bodytext').css({width: innerBlockWidth});
+
+        } else {
+            $('.bodytext').css({width: "90%"});
+        }
+
+        $('.background').css({height: wH});
+        $('.border').css({height: borderHeight});
+        $('.bodytext').css({height: innerBlock});
 
     }
     //Resizes on window resize
     window.onresize = function () {
-            windowH();
+        windowH();
     };
     //On initial load will size
     windowH();
